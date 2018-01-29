3276caea51213c602fe90e7c946727d9a8282ba4
Marcus Baw <marcusbaw@gmail.com>
2018-01-29 14:38:59 +0000

    switch to underscores instead of hyphenation

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='django_opennehr',
+      version='0.0.1',
+      description='openehr archetypes rendered as Django models',
+      url='https://github.com/openhealthcare/django-openehr',
+      author='Open Health Care UK / Dr Marcus Baw / David Miller',
+      license='MIT',
+      packages=['django_openehr'],
+      zip_safe=False)
