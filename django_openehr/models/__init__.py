86b0f89af3d10719dee08cdb70d1486a1ca1f11b
Marcus Baw <marcusbaw@gmail.com>
2018-02-07 17:29:32 +0000

    updated with new and removed models

--- django_openehr/models/__init__.py
+++ django_openehr/models/__init__.py
@@ -1,15 +1,20 @@
 from .address_details import AddressDetails
 from .adverse_reaction import AdverseReaction
+from .body_site import BodySite
 from .demographic_personal import DemographicPersonal
 from .demographic_professional import DemographicProfessional
 from .identifier import Identifier
 from .inpatient_admission import InpatientAdmission
 from .person_name import PersonName
+from .problem_diagnosis import ProblemDiagnosis
 from .reason_for_encounter import ReasonForEncounter
 from .relevant_contact import RelevantContact
-from .symptom_sign import BodySite, SymptomSign
+from .symptom_sign import SymptomSign
 from .telecom_details import TelecomDetails
-from .therapeutic_direction import TherapeuticDirection, TherapeuticDirectionDosage
+from .therapeutic_direction import (
+    TherapeuticDirection,
+    TherapeuticDirectionDosage
+)
 
 __all__ = [
     'AddressDetails',
@@ -20,6 +25,7 @@ __all__ = [
     'Identifier',
     'InpatientAdmission',
     'PersonName',
+    'ProblemDiagnosis',
     'ReasonForEncounter',
     'RelevantContact',
     'SymptomSign',
