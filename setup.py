54ccd3a1a55b91c8b0ffefc97d87a1b26c4bb0da
Jonathan Barratt <jonathan.barratt@gmail.com>
2018-02-16 20:15:04 +0700

    fix github project name in setup url
    s/django_openehr/django-openehr/

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(name='django_openehr',
       version='0.0.3-01',
       description='openehr archetypes rendered as Django models',
-      url='https://github.com/openhealthcare/django_openehr',
+      url='https://github.com/openhealthcare/django-openehr',
       author='Open Health Care UK',
       license='MIT',
       packages=find_packages(),
